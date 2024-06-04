f039ec52e6f582ae963ec3243589d12c0ae8b044
Bruna Eduarda Kohler <134429539+BRUNAEDUARDAKOHLER@users.noreply.github.com>
2024-06-04 11:29:39 -0300

    Atualização

---
+++ svript.js
@@ -0,0 +1,26 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultados = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+    const pergunta1 = [
+        {
+        enunciado: "Pergunta 1",
+        alternativas: ["
+        Alternativa 1",
+        
+        "Alternativa 2"
+    ]
+        "Pergunta 2"
+        }
+        {
+       enunciado: "Pergunta 1",
+       alternativas: ["
+       Alternativa 1",
+    
+       "Alternativa 2"
+]
+       "Pergunta 2"
+        }
+    ]
