8dac6979de873a123806c5f2f10f59f453936102
Bruna Eduarda Kohler <134429539+BRUNAEDUARDAKOHLER@users.noreply.github.com>
2024-08-06 16:55:57 -0300

    Atualização

--- svript.js
+++ svript.js
@@ -6,8 +6,7 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Era uma vez uma pequena cidade chamada Verdelândia, conhecida por suas ruas limpas e belas praças. Contudo, ultimamente, o problema do lixo nas ruas começou a surgir. Vamos acompanhar a história de João, um jovem morador que quer fazer a diferença em sua cidade. Ao longo do caminho, você ajudará João a tomar decisões. Escolha sabiamente, pois cada decisão terá uma consequência."
-        "João acorda cedo em um sábado ensolarado e decide dar uma volta pela cidade. Ele percebe que há muito lixo espalhado pelas ruas e praças.",
+        enunciado: "Era uma vez uma pequena cidade chamada Verdelândia, conhecida por suas ruas limpas e belas praças. Contudo, ultimamente, o problema do lixo nas ruas começou a surgir. Vamos acompanhar a história de João, um jovem morador que quer fazer a diferença em sua cidade. Ao longo do caminho, você ajudará João a tomar decisões. Escolha sabiamente, pois cada decisão terá uma consequência. João acorda cedo em um sábado ensolarado e decide dar uma volta pela cidade. Ele percebe que há muito lixo espalhado pelas ruas e praças.",
         alternativas: [
             {
                 texto: "Ignorar o lixo e continuar seu passeio.",
@@ -53,8 +52,8 @@ const perguntas = [
                 afirmacao: "O prefeito concorda e, após instalar mais lixeiras, a quantidade de lixo nas ruas diminui consideravelmente."
             },
             {
-                texto: "Realizar campanhas educativas sobre a importância de não jogar lixo no chão."
-                afirmacao: " As campanhas educativas são bem-sucedidas e a conscientização dos moradores aumenta, resultando em menos lixo nas ruas."
+                texto: "Realizar campanhas educativas sobre a importância de não jogar lixo no chão.",
+                afirmacao: "As campanhas educativas são bem-sucedidas e a conscientização dos moradores aumenta, resultando em menos lixo nas ruas."
             }
         ]
     },
@@ -63,13 +62,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Organizar mutirões mensais de limpeza.",
-                afirmacao: " Os mutirões mensais se tornam um evento popular e divertido, envolvendo toda a comunidade na limpeza da cidade."
-                "Independente das escolhas, João mostrou que pequenas ações podem ter grandes impactos. Verdelândia aprendeu com seu exemplo e tornou-se uma cidade modelo em termos de limpeza e consciência ambiental. E você, qual seria sua escolha?"
+                afirmacao: " Os mutirões mensais se tornam um evento popular e divertido, envolvendo toda a comunidade na limpeza da cidade. Independente das escolhas, João mostrou que pequenas ações podem ter grandes impactos. Verdelândia aprendeu com seu exemplo e tornou-se uma cidade modelo em termos de limpeza e consciência ambiental. E você, qual seria sua escolha?"
             },
             {
                 texto: "Fundar uma ONG focada na preservação do meio ambiente.",
-                afirmacao: "A ONG de João cresce rapidamente, promovendo várias ações ambientais e projetos educativos, inspirando outras cidades a seguirem o exemplo."
-                "Independente das escolhas, João mostrou que pequenas ações podem ter grandes impactos. Verdelândia aprendeu com seu exemplo e tornou-se uma cidade modelo em termos de limpeza e consciência ambiental. E você, qual seria sua escolha?"
+                afirmacao: "A ONG de João cresce rapidamente, promovendo várias ações ambientais e projetos educativos, inspirando outras cidades a seguirem o exemplo. Independente das escolhas, João mostrou que pequenas ações podem ter grandes impactos. Verdelândia aprendeu com seu exemplo e tornou-se uma cidade modelo em termos de limpeza e consciência ambiental. E você, qual seria sua escolha?"
             }
         ]
     },
@@ -108,7 +105,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "Fim...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
